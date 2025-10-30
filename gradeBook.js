34b454d26ca2dac86ec637dd38c2808447484cfb
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2025-10-30 08:57:50 -0600

    Change expressions for cases to try to fix errors

--- gradeBook.js
+++ gradeBook.js
@@ -3,22 +3,22 @@ function getGrade(s1, s2, s3) {
     var average = (s1 + s2 + s3) / 3
 
     switch (average) {
-        case (90 <= average <= 100):
+        case (average >= 90):
             console.log("A")
             break;
 
-        case (80 <= average < 90):
+        case (average >= 80):
             console.log("B")
             break;
 
-        case (70 <= average < 80):
+        case (average >= 70):
             console.log("C")
             break;
 
-        case (60 <= average < 70):
+        case (average >= 60):
             console.log("D")
             break;
-            
+
         default:
             console.log("F")
             break;
