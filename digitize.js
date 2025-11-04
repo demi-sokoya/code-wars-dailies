ce92552c4defa82ca2ed968cfa0f832ca526b643
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2025-11-04 10:54:48 -0700

    Flesh out logic for the program and test

--- digitize.js
+++ digitize.js
@@ -22,3 +22,22 @@ FUNCTION digitize(n)
     RETURN digits
 END FUNCTION
 */
+
+function digitize(n) {
+    if (n < 0) {
+        console.log("Please give a non-negative number")
+        return []
+    }
+
+
+    let digits = []
+    while (n > 0) { 
+        let digit = n % 10
+        digits.push(digit)
+        n = Math.floor(n / 10)
+    }
+
+    return digits
+}
+
+console.log(digitize(123456))
