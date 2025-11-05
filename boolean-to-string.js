4a0ed15d1318d3ebd70d9be1bbae152f273dc085
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2025-11-05 12:39:49 -0700

    Refine pseudo-code and update function name to boolToWord for clarity

--- boolean-to-string.js
+++ boolean-to-string.js
@@ -1,5 +1,5 @@
-/*Pseudo-code
-FUNCTION boolToString(boolean)
+/*Pseudo-code for Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+FUNCTION boolToWord(boolean)
     IF boolean THEN
         RETURN "Yes"
     ELSE
@@ -8,3 +8,14 @@ FUNCTION boolToString(boolean)
 END FUNCTION
  *      
 */
+
+function boolToWord(bool) {
+    if (typeof bool !== 'boolean') {
+        return "Please use a boolean value(true or false)"
+    } else if(bool === true) {
+        return "Yes"
+    } else {
+        return "No"
+    }
+}
+console.log(boolToWord(null))
