f9a973e4b0fc09d01760797db70eb8366f6d92a3
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2025-11-06 12:58:35 -0700

    Implement basic arithmetic operations function with pseudo-code

---
+++ basic-arithmetic.js
@@ -0,0 +1,24 @@
+//Your task is to create a function that does four basic mathematical operations.
+
+//The function should take three arguments - operation(string/char), value1(number), value2(number).
+//The function should return result of numbers after applying the chosen operation.
+//Pseudo-code
+/*
+FUNCTION basicOp (operation, value1, value2)
+    CASE OF operation
+    '+':
+        RETURN value1 + value2
+    '-':
+        RETURN value1 - value2
+    '*':
+        RETURN value1 * value2
+    '/':
+        RETURN value1 / value2
+    OTHERWISE:
+        OUTPUT "Invalid operation"
+        RETURN NULL
+    END CASE
+END FUNCTION
+    
+*/
+
