973e3e57a1cd381c4417e045444c450cd67a7073
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2025-11-08 11:00:12 -0700

    Add areaOrPerimeter function to calculate area or perimeter of a polygon

---
+++ area-or-perimeter.js
@@ -0,0 +1,13 @@
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+/*
+Pseudo-code
+--------------
+SET areaOrPerimeter TO a function that takes (l, w)
+    IF l = w THEN
+        RETURN l * w
+    ELSE
+        RETURN 2 * (l + w)
+    ENDIF
+END FUNCTION
+*/
