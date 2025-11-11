c0ffa2e95a49247b40bfaf98510b4c6318519bca
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2025-11-11 09:17:23 -0700

    Write out pseudo-code to show program logic

---
+++ remove-first-and-last.js
@@ -0,0 +1,11 @@
+//Your goal is to write a function that removes the first and last characters of a string. You're given one parameter, the original string.
+
+//Important: Your function should handle strings of any length ≥ 2 characters. For strings with exactly 2 characters, return an empty string.
+
+//Pseudo-code:
+// FUNCTION removeChar(str)
+//     IF LENGTH(str) LESS THAN OR EQUALS 2 THEN
+//         RETURN empty string
+//     ENDIF
+//     RETURN SUBSTRING of str FROM position 1 TO position LENGTH(str) - 2
+// END FUNCTION
