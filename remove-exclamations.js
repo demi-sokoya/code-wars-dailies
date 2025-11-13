2d66bcab89eb6d964d1df5a71d79f48d337920ea
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2025-11-13 12:11:05 -0700

    Start with pseudo-code

---
+++ remove-exclamations.js
@@ -0,0 +1,9 @@
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+/*
+Pseudo-code
+------------
+
+FUNCTION removeExclamationMarks(s)
+    RETURN s with all "!" REPLACED by ""
+END FUNCTION
+*/
