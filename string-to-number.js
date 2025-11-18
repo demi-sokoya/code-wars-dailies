4e1f51935f41437f6f2182303a733bad13674208
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2025-11-18 11:16:38 -0700

    Lay out pseudo-code

---
+++ string-to-number.js
@@ -0,0 +1,10 @@
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+/**
+ * #Pseudo-code
+ * ------------
+ *
+ * FUNCTION stringToNumber(str)
+ *      RETURN CONVERT str TO number
+ * END FUNCTION
+ *
+ */
