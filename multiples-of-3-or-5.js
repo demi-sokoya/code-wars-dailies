ff34b0d6fa2015ba58b484411395cc704098dc31
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2025-11-19 11:24:23 -0700

    Implement initial version of the getMultiplesOfThreeAndFive function with pseudo-code

---
+++ multiples-of-3-or-5.js
@@ -0,0 +1,28 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+// Additionally, if the number is negative, return 0.
+// Note: If a number is a multiple of both 3 and 5, only count it once.
+
+/**
+ * #Pseudo-code
+ * ------------
+ *
+ * FUNCTION getMultiplesOfThreeAndFive (number)
+ *      SET array TO []
+ *      IF number is LESS THAN 0 THEN
+ *          RETURN 0
+ *      ENDIF
+ *
+ *      FOR (i = number; i > 0; i--)
+ *          IF i is DIVISIBLE by 3 AND i is DIVISIBLE by 5 THEN
+ *              APPEND i to array
+ *          ELSE IF i is DIVISIBLE by 3 THEN
+ *              APPEND i to array
+ *          ELSE IF i is DIVISIBLE by 5 THEN
+ *              APPEND i to array
+ *          ENDIF
+ *      END FOR
+ *
+ *      RETURN array
+ * END FUNCTION
+ */
